8b0a6ad8338ab9457d64963e996cf71d99ada72d
Adalberto Neves <nevessoka9@gmail.com>
2021-04-06 16:54:28 +0100

    first commit

---
+++ src/test/java/com/ecommerce/angelClothes/AngelClothesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.angelClothes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AngelClothesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
